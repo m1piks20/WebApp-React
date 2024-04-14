35c152bdb6585303d2d562827a51bbc4edc030f9
Belyashik2K <lovelybelyashik@gmail.com>
2024-04-14 03:20:21 +0300

    add i18n

--- src/i18n.js
+++ src/i18n.js
@@ -12,7 +12,7 @@ i18n
     .use (initReactI18next)
     .init({
         // Стандартный язык
-        fallbackLng: 'en',
+        fallbackLng: 'ru',
         debug: true,
         // Распознавание и кэширование языковых кук
         detection: {
