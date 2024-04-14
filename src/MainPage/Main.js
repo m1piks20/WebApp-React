08129ce703453626531ec26cbeaeb86120c38d6b
Belyashik2K <lovelybelyashik@gmail.com>
2024-04-14 04:13:41 +0300

    add i18n

--- src/MainPage/Main.js
+++ src/MainPage/Main.js
@@ -12,6 +12,14 @@ function Main() {
 
     useEffect(() => {
         updateUsername();
+
+        axios.get('https://7c82-178-162-3-38.ngrok-free.app/user/get/5560007118')
+            .then((response) => {
+                i18n.changeLanguage(response.data.lang_code);
+            })
+            .catch((error) => {
+                console.error(error);
+            });
         // Вызываем функцию обновления имени пользователя при загрузке компонента
     }, []);
 
@@ -38,13 +46,7 @@ function Main() {
     const [userData, setUserData] = useState(null);
 
     useEffect(() => {
-        axios.get('https://7c82-178-162-3-38.ngrok-free.app/user/get/5560007118')
-            .then((response) => {
-                i18n.changeLanguage(response.data.lang_code);
-            })
-            .catch((error) => {
-                console.error(error);
-            });
+
     });
 
 
